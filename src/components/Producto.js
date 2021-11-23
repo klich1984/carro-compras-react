b59c7b768badf5240703e9de4249506d8a0004d6
Carlos Usuga Martinez <klich84@gmail.com>
2021-11-22 20:10:42 -0500

    Add components of productos and producto

---
+++ src/components/Producto.js
@@ -0,0 +1,18 @@
+import { Component } from 'react';
+
+class Producto extends Component {
+	render() {
+		// console.log(this.props)
+		// recuperar las propiedades
+		const { producto } = this.props
+		return (
+			<div>
+				<img src={producto.img} alt={producto.name} />
+				<h3>{producto.name}</h3>
+				<p>{producto.price}</p>
+			</div>
+		);
+	}
+}
+
+export default Producto;
