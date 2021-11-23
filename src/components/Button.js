04cc96adfc44e07756247c72c761693ad50f1c5a
Carlos Usuga Martinez <klich84@gmail.com>
2021-11-22 20:41:23 -0500

    add component Button

---
+++ src/components/Button.js
@@ -0,0 +1,23 @@
+import { Component } from 'react'
+
+const styles = {
+	button: {
+		backgroundColor: '#0a283e',
+		color: '#fff',
+		padding: '15px 20px',
+		borderRadius: '5px',
+		borderStyle: 'none',
+		cursor: 'pointer',
+	}
+}
+
+export default class Button extends Component {
+	render() {
+		console.log(this.props)
+		return (
+			<div>
+				<button {...this.props} style={styles.button}/>
+			</div>
+		)
+	}
+}
